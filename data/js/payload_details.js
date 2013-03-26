fcb5f65b1a61fd64a277d63867e841705df4803b
David Anguita <david@davidanguita.name>
2013-03-26 19:57:19 +0100

    This branch is for info purposes only

--- data/js/payload_details.js
+++
@@ -1,5 +0,0 @@
-var payload_details =  {
-  "tweets" : 7563,
-  "created_at" : "Sun Mar 10 16:02:33 +0000 2013",
-  "lang" : "en"
-}
--- data/js/user_details.js
+++
@@ -1,8 +0,0 @@
-var user_details =  {
-  "screen_name" : "danguita",
-  "location" : "Ciudad Real, Spain",
-  "full_name" : "David Anguita",
-  "bio" : "Geek, software developer and consultant. Building software with Ruby on Rails. Partner & lead developer @aticoestudio",
-  "id" : "40325830",
-  "created_at" : "Fri May 15 20:30:18 +0000 2009"
-}
